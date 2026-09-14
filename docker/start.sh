ac105e0c59356763fc7366aeff540da86c7d516b
Sai Ko <sai.ko@mothernode.com>
2026-09-14 10:31:41 -0400

    fix: use bundled prisma for migrations in docker

--- docker/start.sh
+++ docker/start.sh
@@ -4,7 +4,7 @@ set -x
 
 echo "Deploying prisma migrations"
 
-pnpx prisma@6.6.0  migrate deploy --schema ./apps/web/prisma/schema.prisma
+node ./node_modules/prisma/build/index.js migrate deploy --schema ./apps/web/prisma/schema.prisma
 
 echo "Starting web server"
 
